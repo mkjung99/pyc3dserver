9ca0430eba7728d5d90f8c85d5a751108b3985c4
Moon Ki Jung <m.k.jung@outlook.com>
2020-06-22 22:06:36 +0100

    Prepare setup settings
    Prepare setup settings.

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyc3dserver",
+    version="0.0.1",
+    author="Moon Ki Jung",
+    author_email="m.k.jung@outlook.com",
+    description="PyC3Dserver: Python interface of C3Dserver software for reading and editing C3D motion capture files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mkjung99/pyc3dserver",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
